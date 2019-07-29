94d51195ae0df4e4d6569000db2eeb6df87dfd9b
gisellehargrove <gisellehargrove@gmail.com>
2019-07-29 18:43:16 -0400

    updating login

--- disney-parent/src/Components/Login/Login.js
+++ disney-parent/src/Components/Login/Login.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const Login = () => {
+  let url = "https://disneyparents.herokuapp.com/";
+
+  useEffect(() => {
+    axios.get('https://touringplans.com/magic-kingdom/attractions.json').then((response) => {
+      console.log(response);;
+    });
+  }, [])
 
   return (
     <div>Login Component</div>
