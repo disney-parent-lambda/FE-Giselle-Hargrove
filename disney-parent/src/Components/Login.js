06090e175a57b8755a69415bfedb458589944eed
gisellehargrove <gisellehargrove@gmail.com>
2019-07-29 22:15:27 -0400

    updating login page

---
+++ disney-parent/src/Components/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  }
+}));
+
+
+
+export default function Login() {
+
+  return (
+    <form autoComplete="off">
+      <TextField
+        id="outlined-email-input"
+        label="Email"
+        type="email"
+        name="email"
+        autoComplete="email"
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        margin="normal"
+        variant="outlined"
+      />
+    </form>
+  );
+};
