52f4e147702af38a48d21bb06414bd986c849bb1
gisellehargrove <gisellehargrove@gmail.com>
2019-07-30 13:55:30 -0400

    updating login info

--- disney-parent/src/Components/Login.js
+++ disney-parent/src/Components/Login.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -19,24 +20,42 @@ const useStyles = makeStyles(theme => ({
 
 
 export default function Login() {
+  const [creds, setCreds] = useState({username: '', password: ''});
   const classes = useStyles();
 
-  const onSubmit = event => {
-    console.log(event)
+  const handleChange = name => event => {
+    setCreds({...creds, [name]: event.target.value });
   }
 
+  const handleSubmit = e => {
+    console.log(e)
+    axios.post('https://disneyparents.herokuapp.com/login',
+    `grant_type=password&username=${creds.username}&password=${creds.password}`,
+    {
+      headers: {
+        // btoa is converting our client id/client secret into base64
+        Authorization: `Basic ${window.btoa('lambda-client:lambda-secret')}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(res => {
+      console.log(res)
+        /*localStorage.setItem('token', res.data.access_token);*/
+        /*this.props.history.push('/users');*/
+    }).catch(err => console.dir(err));
+      e.preventDefault();
+  };
+
   return (
-    <div className="form-container" onSubmit={onSubmit}>
-      <form className={classes.container} autoComplete="off">
+    <div className="form-container">
+      <form className={classes.container} autoComplete="off" onSubmit={event => handleSubmit(event)}>
         <TextField
           className={classes.textField}
           id="outlined-email-input"
-          label="Email"
-          type="email"
+          label="username"
           name="email"
-          autoComplete="email"
           margin="normal"
           variant="outlined"
+          onChange={handleChange('username')}
         />
         <TextField
           className={classes.textField}
@@ -46,9 +65,10 @@ export default function Login() {
           autoComplete="current-password"
           margin="normal"
           variant="outlined"
+          onChange={handleChange('password')}
         />
+      <button variant="outlined" color="primary" className={classes.button}>Log In</button>
       </form>
-      <Button variant="outlined" color="primary" className={classes.button}>Log In</Button>
     </div>
   );
 };
