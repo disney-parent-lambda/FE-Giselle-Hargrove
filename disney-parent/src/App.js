94732b5cf1f371ef6dae84568fe117046b4514f5
gisellehargrove <gisellehargrove@gmail.com>
2019-07-29 21:51:30 -0400

    added appBar component

--- disney-parent/src/App.js
+++ disney-parent/src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import Login from './Components/Login/Login';
 import { Route } from 'react-router-dom';
+import AppRouter from './Components/AppRouter.js';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      
+      <AppRouter />
     </div>
   );
 }
--- disney-parent/src/Components/AppRouter.js
+++ disney-parent/src/Components/AppRouter.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import AppBar from './AppBar';
+import NavBar from './AppBar';
 
 const AppRouter = () => {
   return (
     <Router>
 
-
-
-      <Route path="" component="" />
-      <Route path="" component="" />
-      <Route path="" component="" />
     </Router>
   );
 };
+
+export default AppRouter;
