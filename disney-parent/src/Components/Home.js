14b3b65280f7cbc0b2aaecc9b7dd796e95734e7d
gisellehargrove <gisellehargrove@gmail.com>
2019-07-30 12:55:38 -0400

    added tabs

--- disney-parent/src/Components/Home.js
+++ disney-parent/src/Components/Home.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import TabPanel from './TabPanel';
+import LinkTab from './LinkTab';
 
 export default function Home() {
-  return <div>Home Component</div>
+  const [value, setValue] = useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function a11yProps(index) {
+    return {
+      id: `nav-tab-${index}`,
+      'aria-controls': `nav-tabpanel-${index}`,
+    };
+  }
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          onChange={handleChange}>
+
+          <LinkTab label="Home" href="/home" {...a11yProps(0)} />
+          <LinkTab label="Open Tickets" href="/tickets" {...a11yProps(1)} />
+          <LinkTab label="Edit Profile" href="/edit-profile" {...a11yProps(2)} />
+
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Page One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Page Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Page Three
+      </TabPanel>
+    </div>
+
+  )
 };
